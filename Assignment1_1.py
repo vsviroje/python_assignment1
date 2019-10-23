3ba7af4562bbae098e668b0c911332db7daa558d
vsviroje <vsviroje@gmail.com>
2019-10-23 11:11:17 +0530

    Assignment1

---
+++ Assignment1_1.py
@@ -0,0 +1,6 @@
+def fun():
+    print("hello from fun");
+
+
+if __name__ == "__main__":
+    fun();
---
+++ Assignment1_2.py
@@ -0,0 +1,10 @@
+def chknum(n):
+    if n%2==0:
+        print("even Number");
+    else:
+        print("odd Number");
+
+
+if __name__=='__main__':
+    n=int(input());
+    chknum(n);
